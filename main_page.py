070532a9884bc7a38192d3034762ae549c9cbbf2
Steve H <s.holman@outlook.com>
2025-08-20 14:45:09 -0700

    Creating dictionary

--- main_page.py
+++ main_page.py
@@ -7,13 +7,11 @@ def create_main_page(page):
 
     return ft.Column([
         ft.Text("Dictionary - The Definition Game", style="headlineMedium"),
+        ft.Divider(),
         ft.FilledTonalButton("New Game", on_click=lambda _: page.go("/game")),
         ft.FilledTonalButton("Settings", on_click=lambda _: page.go("/settings")),
-        ft.FilledTonalButton("Exit", on_click=lambda _: page.window_close()),
+        ft.FilledTonalButton("Exit", on_click=lambda _: page.window.close()),
     ], alignment="center", horizontal_alignment="center")
 
 if __name__ == "__main__":  
     main()
-
-
-                
